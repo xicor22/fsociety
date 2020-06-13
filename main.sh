d1aef5390ea5ce48e13654af0389600aa074547f
Saksham Singh <sakshamsingh.93502@gmail.com>
2020-06-13 23:00:45 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,7 @@
 echo "Installing FSociety......" && cd $HOME
 git clone https://github.com/xicor22/fsociety >/dev/null 2>&1
+cd fsociety
 sudo ./install.sh 
+cd $HOME
 rm main.sh
-echo "DONE! Type 'fsociety' to call the runn the program."
+echo "DONE! Type 'fsociety' to call the run the program."
